633d553da92ad3e870c575537d67ebccfb58c416
Roman Heinrich <roman.heinrich@gmail.com>
2021-01-31 00:26:08 +0100

    Add Phoenix app example

---
+++ examples/demo/config/config.exs
@@ -0,0 +1,28 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+# Configures the endpoint
+config :demo, DemoWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "A0xid91CuqIR01sqbAwgJFXmIeQG2dPcRCOarSLHpppgUwOA5kxc5Yn04slpih3X",
+  render_errors: [view: DemoWeb.ErrorView, accepts: ~w(html json), layout: false],
+  pubsub_server: Demo.PubSub,
+  live_view: [signing_salt: "CbTNUYgA"]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
