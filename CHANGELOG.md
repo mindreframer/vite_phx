00ec2c516651aab2dc81b6ddfd3703a2744f51c6
Roman Heinrich <roman.heinrich@gmail.com>
2021-01-30 23:41:45 +0100

    Prepare docs folder + add licence and changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# 0.1.0
+
+- Basic parsing for Vite manifests implemented
+
+
--- mix.exs
+++ mix.exs
@@ -1,13 +1,30 @@
 defmodule Vite.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @elixir_requirement "~> 1.9"
+
+
   def project do
     [
       app: :vite_phx,
-      version: "0.1.0",
-      elixir: "~> 1.1",
+      version: @version,
+      elixir: @elixir_requirement,
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      preferred_cli_env: [docs: :docs],
+      deps: deps(),
+      docs: docs(),
+      package: package(),
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Roman Heinrich"],
+      licenses: ["MIT"],
+      links: %{github: "https://github.com/mindreframer/vite_phx"},
+      files:
+        ~w(lib priv CHANGELOG.md LICENSE.md mix.exs README.md .formatter.exs)
     ]
   end
 
@@ -22,7 +39,10 @@ defmodule Vite.MixProject do
     [
       {:phoenix, ">= 0.0.0", only: [:dev, :test]},
       {:jason, ">= 0.0.0", only: [:dev, :test]},
-      {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
+
+      # Docs dependencies (some for cross references)
+      {:ex_doc, "~> 0.22", only: :docs},
     ]
   end
 end
