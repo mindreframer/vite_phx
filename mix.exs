f76aa69e251ebed0b17a751a1046804ee859434d
Roman Heinrich <roman.heinrich@gmail.com>
2021-01-30 22:58:10 +0100

    Make Elixir version requirement less strict

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Vite.MixProject do
     [
       app: :vite_phx,
       version: "0.1.0",
-      elixir: "~> 1.11",
+      elixir: "~> 1.1",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
