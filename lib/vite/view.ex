b6a65c4bf4882193a319a9957a8932a54e6724c1
Roman Heinrich <roman.heinrich@gmail.com>
2021-01-31 04:25:13 +0100

    Introduce Vite.View to generate script links

---
+++ lib/vite/view.ex
@@ -0,0 +1,42 @@
+defmodule Vite.View do
+  @moduledoc """
+  Help with View integration into Phoenix views
+  """
+  alias Vite.Entry
+
+  @doc """
+  Generate all links for an entry struct in following order:
+
+    1. styles to prevent FOUC
+    2. main entry script
+    3. imports
+
+  `
+  <link phx-track-static rel="stylesheet" href="<%= Vite.Manifest.main_css() %>"/>
+  <script type="module" crossorigin defer phx-track-static src="<%=  Vite.Manifest.main_js() %>"></script>
+  <link rel="modulepreload" href="<%= Vite.Manifest.vendor_js() %>">
+  `
+  """
+  @spec for_entry(Entry.t(), binary()) :: binary()
+  def for_entry(entry = %Entry{}, prefix \\ "/") do
+    script = entry.file |> module_script(prefix)
+    css_files = entry.cssfiles |> Enum.map(&css_link(&1, prefix)) |> Enum.join("\n")
+    imports = entry.imports |> Enum.map(&module_preload(&1, prefix)) |> Enum.join("\n")
+    [css_files, script, imports] |> Enum.join("\n")
+  end
+
+  @spec module_preload(binary(), binary()) :: binary()
+  def module_preload(href, prefix) do
+    ~s(<link rel="modulepreload" href="#{prefix <> href}">)
+  end
+
+  @spec css_link(binary(), binary()) :: binary()
+  def css_link(href, prefix) do
+    ~s{<link phx-track-static rel="stylesheet" href="#{prefix <> href}"/>}
+  end
+
+  @spec module_script(binary(), binary()) :: binary()
+  def module_script(src, prefix) do
+    ~s{<script type="module" crossorigin defer phx-track-static src="#{prefix <> src}"></script>}
+  end
+end
---
+++ test/view_test.exs
@@ -0,0 +1,53 @@
+defmodule Vite.ViewTest do
+  use ExUnit.Case
+  alias Vite.View
+  alias Vite.Entry
+
+  def entry(1) do
+    %Entry{
+      name: "src/main.tsx",
+      file: "assets/main.9160cfe1.js",
+      cssfiles: ["assets/main.c14674d5.css"],
+      imports: ["assets/vendor.3b127d10.js"]
+    }
+  end
+
+  def entry(2) do
+    %Entry{
+      name: "src/main.js",
+      file: "assets/main.9160cfe1.js",
+      cssfiles: ["assets/main.c14674d5.css", "assets/main.c33345b3.css"],
+      imports: ["assets/vendor.3b127d10.js", "assets/vendor.bbddaa33.js"]
+    }
+  end
+
+  def no_lf(s), do: String.replace(s, "\n", "")
+  def no_indent(s), do: Regex.replace(~r/^\s+/m, s, "")
+  def strip(s), do: s |> no_indent() |> no_lf()
+
+  describe "for_entry/1" do
+    test "generates the complete markup to include scripts (1)" do
+      e = entry(1)
+
+      assert View.for_entry(e) |> strip() ==
+               ~S{
+          <link phx-track-static rel="stylesheet" href="/assets/main.c14674d5.css"/>
+          <script type="module" crossorigin defer phx-track-static src="/assets/main.9160cfe1.js"></script>
+          <link rel="modulepreload" href="/assets/vendor.3b127d10.js">
+        } |> strip()
+    end
+
+    test "generates the complete markup to include scripts (2)" do
+      e = entry(2)
+
+      assert View.for_entry(e) |> strip() ==
+               ~S{
+          <link phx-track-static rel="stylesheet" href="/assets/main.c14674d5.css"/>
+          <link phx-track-static rel="stylesheet" href="/assets/main.c33345b3.css"/>
+          <script type="module" crossorigin defer phx-track-static src="/assets/main.9160cfe1.js"></script>
+          <link rel="modulepreload" href="/assets/vendor.3b127d10.js">
+          <link rel="modulepreload" href="/assets/vendor.bbddaa33.js">
+        } |> strip()
+    end
+  end
+end
