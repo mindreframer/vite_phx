693d903967b544269aba94e0305600859719a259
Roman Heinrich <roman.heinrich@gmail.com>
2021-02-02 15:11:47 +0100

    Include delegates to the top-level Vite module

--- lib/vite.ex
+++ lib/vite.ex
@@ -2,25 +2,8 @@ defmodule Vite do
   @moduledoc """
   Documentation for `Vite`.
   """
-  alias Vite.{Config, Manifest}
+  alias Vite.View
 
-  @spec main_js() :: binary()
-  def main_js() do
-    Manifest.get_file(Config.main_file()) |> prepend_slash()
-  end
-
-  @spec main_css() :: [binary()]
-  def main_css() do
-    Manifest.get_css(Config.main_file()) |> Enum.map(&prepend_slash/1)
-  end
-
-  @spec vendor_js() :: binary()
-  def vendor_js() do
-    Manifest.get_imports(Config.main_file()) |> Enum.at(0) |> prepend_slash()
-  end
-
-  @spec prepend_slash(binary()) :: binary()
-  defp prepend_slash(result) do
-    "/" <> result
-  end
+  defdelegate vite_client, to: View
+  defdelegate vite_snippet(entry_name), to: View
 end
