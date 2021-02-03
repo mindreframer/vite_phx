afc9fbe174a557f8adaa2323db00295c0d49d555
Roman Heinrich <roman.heinrich@gmail.com>
2021-02-03 14:31:22 +0100

    Add some example pictures to test asset digesting with vite

--- examples/demo/assets/src/app.tsx
+++ examples/demo/assets/src/app.tsx
@@ -2,9 +2,14 @@ import { h, Fragment } from 'preact'
 import { Logo } from './logo'
 
 export function App() {
+  let images = ["162100", "180116", "180311", "180548"]
+  let imgContainers = images.map((value, idx) => {
+    return <img src={`images/${value}.jpg`} height={50} style="padding: 5px;"/>
+  })
   return (
     <>
       <p>Hello Vite + Preact!</p>
+      {imgContainers}
     </>
   )
 }
