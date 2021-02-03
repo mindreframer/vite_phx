848db52f1a0d1943296e00f96385409da7d2e2f7
Roman Heinrich <roman.heinrich@gmail.com>
2021-02-03 15:20:25 +0100

    Also include an image with non-dynamic src

--- examples/demo/assets/src/app.tsx
+++ examples/demo/assets/src/app.tsx
@@ -5,13 +5,15 @@ export function App() {
   let images = ["180116", "180311", "180548", "184010"];
   let imgContainers = images.map((value, idx) => {
     return (
-      <img src={`images/${value}.jpg`} height={50} style="padding: 5px;" />
+      <img src={`/images/${value}.jpg`} height={50} style="padding: 5px;" />
     );
   });
   return (
     <>
       <p>Hello Vite + Preact!</p>
       {imgContainers}
+      <br/>
+      <img src="/images/phoenix.png" height={50} style="padding: 5px;" />
     </>
   );
 }
