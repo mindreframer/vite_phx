5ecee2824fd30ce81788faebf678b9b018b25cd4
Roman Heinrich <roman.heinrich@gmail.com>
2021-02-03 14:47:52 +0100

    Prettier formatting

--- examples/demo/assets/src/app.tsx
+++ examples/demo/assets/src/app.tsx
@@ -1,15 +1,17 @@
-import { h, Fragment } from 'preact'
-import { Logo } from './logo'
+import { h, Fragment } from "preact";
+import { Logo } from "./logo";
 
 export function App() {
-  let images = ["162100", "180116", "180311", "180548"]
+  let images = ["180116", "180311", "180548", "184010"];
   let imgContainers = images.map((value, idx) => {
-    return <img src={`images/${value}.jpg`} height={50} style="padding: 5px;"/>
-  })
+    return (
+      <img src={`images/${value}.jpg`} height={50} style="padding: 5px;" />
+    );
+  });
   return (
     <>
       <p>Hello Vite + Preact!</p>
       {imgContainers}
     </>
-  )
+  );
 }
